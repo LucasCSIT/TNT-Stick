3abe0b119e7a8eb5768a5a8807cce401806b0f81
Lucas Garcia <lucasisawesome@icloud.com>
2026-02-03 23:46:05 -0500

    begin implementation of explodeAroundPlayer

--- src/main/java/io/papermc/tntStick/TNTStick.java
+++ src/main/java/io/papermc/tntStick/TNTStick.java
@@ -7,54 +7,84 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.entity.EntityDamageEvent;import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.Material;
 
 /**
  * {@code TNTStick} is a plugin for Minecraft that grants the wielder of a stick abilities related to TNT.
+ * The player is immune to all explosions.
  * @author Lucas Garcia
- * @version 0.0.1
+ * @version 0.2.0
  */
 public class TNTStick extends JavaPlugin implements Listener {
-    @Override
-    public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(this, this);
+  @Override
+  public void onEnable() {
+    Bukkit.getPluginManager().registerEvents(this, this);
+  }
+
+  /**
+   * Events related to the player holding a stick in their main hand.
+   * @param event
+   */
+  @EventHandler
+  private void onPlayerHasStick(PlayerInteractEvent event) {
+    Action action = event.getAction();
+    Player player = event.getPlayer();
+
+    if (!isStickInMainHand(player)) {
+      return;
     }
 
-    /**
-     * Events related to the player holding a stick in their main hand.
-     * @param event
-     */
-    @EventHandler
-    private void onPlayerHasStick(PlayerInteractEvent event) {
-      Action action = event.getAction();
-      Player player = event.getPlayer();
+    if (action.equals(Action.LEFT_CLICK_AIR) || action.equals(Action.LEFT_CLICK_BLOCK)) {
+      fireTNT(player);
+    }
 
-      if (!isStickInMainHand(player)) {
-        return;
+    if (action.equals(Action.RIGHT_CLICK_AIR) || action.equals(Action.RIGHT_CLICK_BLOCK)) {
+      explodeAroundPlayer(player);
+    }
+  }
+
+  /**
+   * Events related to the player taking explosive damage due to the TNT Stick.
+   * @param event
+   */
+  @EventHandler
+  private void onPlayerTakeExplosionDamage(EntityDamageEvent event) {
+    if (event.getEntity() instanceof Player player) {
+      Material mainHandItem = player.getInventory().getItemInMainHand().getType();
+      if (event.getCause() == EntityDamageEvent.DamageCause.BLOCK_EXPLOSION && mainHandItem == Material.STICK) {
+        event.setCancelled(true);
       }
-
-      if (action.equals(Action.LEFT_CLICK_AIR)) {
-        fireTNT(player);
-      }
     }
+  }
 
-    /**
-     * Shoots out TNT in front of the player. The TNT explodes after a short time.
-     * @param player
-     */
-    private void fireTNT(Player player) {
-      Entity entity = player.getWorld().spawnEntity(player.getEyeLocation(), EntityType.TNT);
-      entity.setVelocity(player.getLocation().getDirection().multiply(3));
-    }
+  /**
+   * Shoots out TNT in front of the player. The TNT explodes after a short time.
+   * @param player
+   */
+  private void fireTNT (Player player){
+    Entity entity = player.getWorld().spawnEntity(player.getEyeLocation(), EntityType.TNT);
+    entity.setVelocity(player.getLocation().getDirection().multiply(3));
+  }
 
-    /**
-     * Checks if the player is holding a stick in their main hand.
-     * @param player
-     * @return {@code True} if the item in the player's main hand is a stick.
-     */
-    private boolean isStickInMainHand(Player player) {
-      return player.getInventory().getItemInMainHand().getType() == Material.STICK;
-    }
+  /**
+   * Creates an explosion around the player.
+   * @param player
+   */
+  private void explodeAroundPlayer(Player player){
+    // TODO: Explosion occurring 3 times. Should be just once.
+    player.getWorld().createExplosion(player.getLocation(), 10);
+    player.sendMessage("exploded");
+    return;
+  }
+
+  /**
+   * Checks if the player is holding a stick in their main hand.
+   * @param player
+   * @return {@code True} if the item in the player's main hand is a stick.
+   */
+  private boolean isStickInMainHand (Player player){
+    return player.getInventory().getItemInMainHand().getType() == Material.STICK;
+  }
 }
