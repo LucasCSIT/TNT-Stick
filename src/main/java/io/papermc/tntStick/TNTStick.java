61a217f69aa57bbdd90283d30a0c574ab95b45d4
Lucas Garcia <lucasisawesome@icloud.com>
2026-02-03 22:59:51 -0500

    add javadoc for methods and remove unneeded parameter for fireTNT method

--- src/main/java/io/papermc/tntStick/TNTStick.java
+++ src/main/java/io/papermc/tntStick/TNTStick.java
@@ -11,12 +11,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.Material;
 
+/**
+ * {@code TNTStick} is a plugin for Minecraft that grants the wielder of a stick abilities related to TNT.
+ * @author Lucas Garcia
+ * @version 0.0.1
+ */
 public class TNTStick extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
+    /**
+     * Events related to the player holding a stick in their main hand.
+     * @param event
+     */
     @EventHandler
     private void onPlayerHasStick(PlayerInteractEvent event) {
       Action action = event.getAction();
@@ -27,16 +36,24 @@ public class TNTStick extends JavaPlugin implements Listener {
       }
 
       if (action.equals(Action.LEFT_CLICK_AIR)) {
-        fireTNT(event, player);
+        fireTNT(player);
       }
     }
 
-    private void fireTNT(PlayerInteractEvent event, Player player) {
-      Entity entity = event.getPlayer().getWorld().spawnEntity(player.getEyeLocation(), EntityType.TNT);
-
+    /**
+     * Shoots out TNT in front of the player. The TNT explodes after a short time.
+     * @param player
+     */
+    private void fireTNT(Player player) {
+      Entity entity = player.getWorld().spawnEntity(player.getEyeLocation(), EntityType.TNT);
       entity.setVelocity(player.getLocation().getDirection().multiply(3));
     }
 
+    /**
+     * Checks if the player is holding a stick in their main hand.
+     * @param player
+     * @return {@code True} if the item in the player's main hand is a stick.
+     */
     private boolean isStickInMainHand(Player player) {
       return player.getInventory().getItemInMainHand().getType() == Material.STICK;
     }
